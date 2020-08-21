1b743fd778267371f95eabbaee5095bc8803061a
Junaid Atari <mj.atari@gmail.com>
2020-08-21 21:59:42 +0500

    create functions, improve code style

--- src/utils/random.js
+++ src/utils/random.js
@@ -12,6 +12,9 @@ import randomFloat from 'random-float';
 import randomInt from 'random-int';
 import uniqueRandomArray from 'unique-random-array';
 
+// Utils
+import * as Defaults from './defaults';
+
 /** Builtin environments */
 export const environments: Array<string> = [
 	'eurasia', 'eurasia_conflict', 'eurasia_flatlands',
@@ -30,39 +33,82 @@ export const trees: Array<string> = [
 	'Strawberry', 'Chestnut', 'Pear', 'Cherry', 'Service',
 ];
 
+/** Random Frequency */
+export function randomFrequency ( fraction: number = 2 ): number {
+	return Number(randomFloat(0, 1)).toFixed(fraction);
+}
+
+/** Random Density */
+export function randomDensity ( fraction: number = 2 ): number {
+	return Number(randomFloat(Defaults.DENSITY_MIN, Defaults.DENSITY_MAX)).toFixed(fraction);
+}
+
+/** Random Coordinates */
+export function randomCord ( fraction: number = 2 ): number {
+	return Number(randomFloat(0, 1)).toFixed(fraction);
+}
+
+/** Random Angle */
+export function randomAngle (): [number, number] {
+	return [
+		randomInt(Defaults.ANGLE_MIN, 40),
+		randomInt(41, Defaults.ANGLE_MAX)
+	];
+}
+
+/** Random Altitude */
+export function randomAltitude (): [number, number] {
+	return [
+		randomInt(Defaults.ALTITUDE_MIN, 10),
+		randomInt(11, Defaults.ALTITUDE_MAX)
+	];
+}
+
+/** Random Name */
+export function randomName (): string {
+	return slugify(faker.address.city('{{name.cityPrefix}}').toLowerCase(),'_');
+}
+
+/** Random Seed */
+export function randomSeed (): string {
+	return String(randomInt(0, 11111111)).padStart(8, '0');
+}
+
+/** Random Environment */
+export function randomEnvironment (): string {
+	return faker.random.arrayElement(environments);
+}
+
+/** Random Lakes */
+export function randomLakes (): number {
+	return randomInt(0, 9);
+}
+
+/** Random River */
+export function randomRiver (): boolean {
+	return faker.random.arrayElement([true, false]);
+}
+
 /**
  * @param {?number} value=null - Value to set / Set null for random values
  * @param {number} fraction=2 - Fraction digits
  * @returns {{[string]: number}}
  */
-export const randomFrequencies: ( value?: ?number, fraction?: number ) => {[string]: number}
-	= ( value: ?number = null, fraction: number = 2 ): {[string]: number} => ({
-	freq1: value === null ? randomFrequency(fraction) : Number(value),
-	freq2: value === null ? randomFrequency(fraction) : Number(value),
-	freq3: value === null ? randomFrequency(fraction) : Number(value),
-	freq4: value === null ? randomFrequency(fraction) : Number(value),
-	freq5: value === null ? randomFrequency(fraction) : Number(value),
-	freq6: value === null ? randomFrequency(fraction) : Number(value),
-	freq7: value === null ? randomFrequency(fraction) : Number(value),
-	freq8: value === null ? randomFrequency(fraction) : Number(value),
-});
+export function randomFrequencies ( value: ?number = null, fraction: number = 2 ): {[string]: number} {
+	return {
+		freq1: value === null ? randomFrequency(fraction) : Number(value),
+		freq2: value === null ? randomFrequency(fraction) : Number(value),
+		freq3: value === null ? randomFrequency(fraction) : Number(value),
+		freq4: value === null ? randomFrequency(fraction) : Number(value),
+		freq5: value === null ? randomFrequency(fraction) : Number(value),
+		freq6: value === null ? randomFrequency(fraction) : Number(value),
+		freq7: value === null ? randomFrequency(fraction) : Number(value),
+		freq8: value === null ? randomFrequency(fraction) : Number(value),
+	};
+}
 
-/** Random Frequency */
-export const randomFrequency: (fraction?: number) => number = (fraction: number = 2): number => Number(randomFloat(0, 1)).toFixed(fraction);
-/** Random Coordinates */
-export const randomCord: (fraction?: number) => number = (fraction: number = 2): number => Number(randomFloat(0, 1)).toFixed(fraction);
-/** Random Name */
-export const randomName: () => string = (): string => slugify(faker.address.city('{{name.cityPrefix}}').toLowerCase(),'_');
-/** Random Seed */
-export const randomSeed: () => string = () => String(randomInt(0, 11111111)).padStart(8, '0');
-/** Random Environment */
-export const randomEnvironment: () => string = (): string => faker.random.arrayElement(environments);
-/** Random Lakes */
-export const randomLakes: () => number = (): number => randomInt(0, 9);
-/** Random River */
-export const randomRiver: () => boolean = (): boolean => faker.random.arrayElement([true, false]);
 /** Random Deposits */
-export const randomDeposits: ( counts?: number ) => boolean = ( counts: number = 2 ): Array<string> => {
+export function randomDeposits ( counts: number = 2 ): Array<string> {
 	const rand: Function = uniqueRandomArray(deposits);
 	const list: Array<string> = [];
 	
@@ -71,9 +117,10 @@ export const randomDeposits: ( counts?: number ) => boolean = ( counts: number =
 	}
 	
 	return list;
-};
+}
+
 /** Random Trees */
-export const randomTrees: ( counts?: number ) => boolean = ( counts: number = 5 ): Array<string> => {
+export function randomTrees ( counts: number = 5 ): Array<string> {
 	const rand: Function = uniqueRandomArray(trees);
 	const list: Array<string> = [];
 	
@@ -82,4 +129,4 @@ export const randomTrees: ( counts?: number ) => boolean = ( counts: number = 5 
 	}
 	
 	return list;
-};
+}
