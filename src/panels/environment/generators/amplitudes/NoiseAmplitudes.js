f7c5e5908bb11abd6515acf0d351d7b9f7a9fd5a
Junaid Atari <mj.atari@gmail.com>
2020-08-24 20:27:25 +0500

    re-write component, make use of hooks

--- src/panels/environment/generators/amplitudes/NoiseAmplitudes.js
+++ src/panels/environment/generators/amplitudes/NoiseAmplitudes.js
@@ -17,207 +17,197 @@ import UiSlider from './../../../../components/UiSlider';
 import * as random from '../../../../utils/random';
 
 /**
- * NoiseAmplitudes `props` type
- * @type {Object}
- */
-type Props = {
-	onChange ( template: string, values?: {[string]: any} ): void,
-};
-
-/**
- * NoiseAmplitudes `state` type
- * @type {Object}
- */
-type State = {
+ * Frequencies type
+ * @type {Object} */
+type ValueFrequencies = {
 	freq1: number, freq2: number, freq3: number, freq4: number,
 	freq5: number, freq6: number, freq7: number, freq8: number,
 };
 
-const fraction: number = 3;
-
 /**
- * NoiseAmplitudes component class
+ * NoiseAmplitudes `props` type
+ * @type {Object}
  */
-export class NoiseAmplitudes extends React.Component<Props, State> {
-	state: State = {
-		...random.randomFrequencies(null, fraction)
+type Props = {
+	frequencies?: ValueFrequencies,
+	onChange ( template: string, value: ValueFrequencies ): void,
+};
+
+/** NoiseAmplitudes functional component */
+function NoiseAmplitudes ( props: Props ) {
+	const [frequencies, setFrequencies] = React.useState<ValueFrequencies>(props.frequencies);
+	
+	// Reflect attributes changes
+	React.useEffect(() => {
+		setFrequencies({...props.frequencies});
+	}, [props.frequencies]);
+	
+	// Reflect state changes
+	React.useEffect(() => {
+		typeof props.onChange === 'function' && props.onChange(toTemplateText(), frequencies);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [frequencies]);
+	
+	/** Generate xml code */
+	const toTemplateText = React.useCallback((): string => {
+		return `<noise_amplitudes values="${Object.values(frequencies).join(' ')}"/>`;
+	}, [frequencies]);
+	
+	const setFrequency = ( name: string, value: number ): void => {
+		setFrequencies(current => ({
+			...current,
+			[name]: value,
+		}))
 	};
 	
-	randomizeValues (): void {
-		this.setState({
-			...random.randomFrequencies(null, fraction)
-		});
-	}
-	
-	resetValues (): void {
-		this.setState({
-			...random.randomFrequencies(0, fraction)
-		});
-	}
-	
-	componentDidMount (): void {
-		const {onChange} = this.props;
-		setTimeout(() => {
-			typeof onChange === 'function'
-				&& onChange(this.toTemplateText(), this.getValues());
-		}, 300);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	componentDidUpdate ( prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS ): void {
-		if ( JSON.stringify(this.state) !== JSON.stringify(prevState) ) {
-			const {onChange} = this.props;
-			
-			setTimeout(() => {
-				typeof onChange === 'function'
-				&& onChange(this.toTemplateText(), this.getValues());
-			}, 300);
-		}
-	}
-	
-	toTemplateText (): string {
-		const {
-			freq1, freq2, freq3, freq4,
-			freq5, freq6, freq7, freq8,
-		} = this.state;
-		
-		const values: string = [
-			freq1, freq2, freq3,
-			freq4, freq5, freq6,
-			freq7, freq8,
-		].join(' ');
-		
-		return `<noise_amplitudes values="${values}"/>`;
-	}
-	
-	getValues (): {[string]: number} {
-		const {
-			freq1, freq2, freq3, freq4,
-			freq5, freq6, freq7, freq8,
-		} = this.state;
-		
-		return {
-			freq1, freq2, freq3, freq4,
-			freq5, freq6, freq7, freq8,
-		};
-	}
-	
-	render () {
-		const {
-			freq1, freq2, freq3, freq4,
-			freq5, freq6, freq7, freq8,
-		} = this.state;
-		
-		return (
-			<>
-				<div className="text-size-xxs text-muted mb-1">
-					Modify the amplitudes of the different noise layers, the initial numbers
-					define the amplitudes of the low-frequencies (the height of large mountains), and the later
-					numbers define the amplitudes of the high_frequencies (the height of the small bumps).
-				</div>
-				<div className="mb-2">
-					Trees und flint south: <code className="pl-2 text-size-xs">{freq1}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq1: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq1: 0})}>Reset</Button>
-					<UiSlider value={Number(freq1)} onChange={v => this.setState({freq1: v})}/>
-				</div>
-				<div className="mb-2">
-					Trees und flint north: <code className="pl-2 text-size-xs">{freq2}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq2: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq2: 0})}>Reset</Button>
-					<UiSlider value={Number(freq2)} onChange={v => this.setState({freq2: v})}/>
-				</div>
-				<div className="mb-2">
-					Hills and monolith: <code className="pl-2 text-size-xs">{freq3}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq3: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq3: 0})}>Reset</Button>
-					<UiSlider value={Number(freq3)} onChange={v => this.setState({freq3: v})}/>
-				</div>
-				<div className="mb-2">
-					Trees near the river: <code className="pl-2 text-size-xs">{freq4}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq4: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq4: 0})}>Reset</Button>
-					<UiSlider value={Number(freq4)} onChange={v => this.setState({freq4: v})}/>
-				</div>
-				<div className="mb-2">
-					Stones: <code className="pl-2 text-size-xs">{freq5}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq5: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq5: 0})}>Reset</Button>
-					<UiSlider value={Number(freq5)} onChange={v => this.setState({freq5: v})}/>
-				</div>
-				<div className="mb-2">
-					Mountains: <code className="pl-2 text-size-xs">{freq6}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq6: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq6: 0})}>Reset</Button>
-					<UiSlider value={Number(freq6)} onChange={v => this.setState({freq6: v})}/>
-				</div>
-				<div className="mb-2">
-					Mountains: <code className="pl-2 text-size-xs">{freq7}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq7: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq7: 0})}>Reset</Button>
-					<UiSlider value={Number(freq7)} onChange={v => this.setState({freq7: v})}/>
-				</div>
-				<div className="mb-2">
-					Metal?: <code className="pl-2 text-size-xs">{freq8}</code>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq8: random.randomFrequency(fraction)})}>
-						Random
-					</Button>
-					<Button className="button-reset-sm" variant="link"
-						onClick={() => this.setState({freq8: 0})}>Reset</Button>
-					<UiSlider value={Number(freq8)} onChange={v => this.setState({freq8: v})}/>
-				</div>
-				<div className="mt-2">
-					<ButtonGroup>
-						<Button variant="secondary" size="sm"
-							onClick={this.randomizeValues.bind(this)}>Randomize All</Button>
-						<Button variant="secondary" size="sm"
-							onClick={this.resetValues.bind(this)}>Reset All</Button>
-					</ButtonGroup>
-				</div>
-			</>
-		);
-	};
+	return (
+		<>
+			<div className="text-size-xxs text-muted mb-1">
+				Modify the amplitudes of the different noise layers, the initial numbers
+				define the amplitudes of the low-frequencies (the height of large mountains), and the later
+				numbers define the amplitudes of the high_frequencies (the height of the small bumps).
+			</div>
+			<div className="mb-2">
+				Frequency 1: <code className="pl-2 text-size-xs">{frequencies.freq1}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq1', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq1', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq1', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq1} onChange={v => setFrequency('freq1', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 2: <code className="pl-2 text-size-xs">{frequencies.freq2}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq2', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq2', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq2', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq2} onChange={v => setFrequency('freq2', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 3: <code className="pl-2 text-size-xs">{frequencies.freq3}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq3', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq3', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq3', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq3}
+					onChange={v => setFrequency('freq3', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 4: <code className="pl-2 text-size-xs">{frequencies.freq4}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq4', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq4', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq4', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq4}
+					onChange={v => setFrequency('freq4', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 5: <code className="pl-2 text-size-xs">{frequencies.freq5}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq5', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq5', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq5', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq5}
+					onChange={v => setFrequency('freq5', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 6: <code className="pl-2 text-size-xs">{frequencies.freq6}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq6', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq6', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq6', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq6}
+					onChange={v => setFrequency('freq6', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 7: <code className="pl-2 text-size-xs">{frequencies.freq7}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq7', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq7', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq7', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq7}
+					onChange={v => setFrequency('freq7', v)}/>
+			</div>
+			<div className="mb-2">
+				Frequency 8: <code className="pl-2 text-size-xs">{frequencies.freq8}</code>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq8', random.randomFrequency())}>
+					Random
+				</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq8', 0)}>Min</Button>
+				<Button className="button-reset-sm" variant="link"
+					onClick={() => setFrequency('freq8', 1)}>Max</Button>
+				<UiSlider step={0.001} value={frequencies.freq8}
+					onChange={v => setFrequency('freq8', v)}/>
+			</div>
+			<div className="mt-2">
+				<ButtonGroup>
+					<Button variant="secondary" size="sm"
+						onClick={() => {
+							setFrequencies({...random.randomFrequencies(null)})
+						}}>Randomize All</Button>
+					<Button variant="secondary" size="sm"
+						onClick={() => {
+							setFrequencies({...random.randomFrequencies(0)})
+						}}>Min All</Button>
+					<Button variant="secondary" size="sm"
+						onClick={() => {
+							setFrequencies({...random.randomFrequencies(1)})
+						}}>Max All</Button>
+				</ButtonGroup>
+			</div>
+		</>
+	);
 }
 
 // Properties validation
-NoiseAmplitudes.defaultProps = {
-	onChange: () => {},
+NoiseAmplitudes.propTypes = {
+	frequencies: PropTypes.shape({
+		freq1: PropTypes.number,
+		freq2: PropTypes.number,
+		freq3: PropTypes.number,
+		freq4: PropTypes.number,
+		freq5: PropTypes.number,
+		freq6: PropTypes.number,
+		freq7: PropTypes.number,
+		freq8: PropTypes.number,
+	}),
+	onChange: PropTypes.func,
 };
 
 // Default properties
-NoiseAmplitudes.propTypes = {
-	onChange: PropTypes.func,
+NoiseAmplitudes.defaultProps = {
+	frequencies: {...random.randomFrequencies(null, 3)},
+	onChange: () => {},
 };
 
 export default NoiseAmplitudes;
