b5cc3c7c3d1c7fe158d47fb4076f0a82d2b1b741
Junaid Atari <mj.atari@gmail.com>
2020-08-27 16:59:45 +0500

    re-write component, make use of hooks

--- src/panels/environment/EnvironmentContainer.js
+++ src/panels/environment/EnvironmentContainer.js
@@ -27,196 +27,140 @@ import TreesPanel from './generators/trees/TreesPanel';
 import DepositPanel from './generators/deposit/DepositPanel';
 import DetailPanel from './generators/detail/DetailPanel';
 import PropPanel from './generators/prop/PropPanel';
-import Seasons from './generators/seasons/Seasons';
+import SeasonsPanel from './generators/seasons/SeasonsPanel';
 
-/**
- * EnvironmentContainer `props` type
- * @type {Object}
- */
-type Props = {
-}
-
-/**
- * EnvironmentContainer `state` type
- * @type {Object}
- */
-type State = {
-	noiseAmplitudes: string,
-	distanceHeightOffset: string,
-	fordDistanceFactor: string,
-	sunAngleFactor: string,
-	resourceFactor: string,
-	deposits: string,
-	trees: string,
-	seasons: string,
-	detail: string,
-	props: string,
-};
-
-/**
- * EnvironmentContainer component
- */
-class EnvironmentContainer extends React.Component<Props, State> {
-	state: State = {
-		noiseAmplitudes: '',
-		distanceHeightOffset: '',
-		fordDistanceFactor: '',
-		backdropScale: '',
-		sunAngleFactor: '',
-		resourceFactor: '',
-		deposits: '',
-		trees: '',
-		seasons: '',
-		detail: '',
-		props: '',
-	};
+/** EnvironmentContainer functional component */
+function EnvironmentContainer (): Node {
+	const [noiseAmplitudes, setNoiseAmplitudes] = React.useState<string>('');
+	const [distanceHeightOffset, setDistanceHeightOffset] = React.useState<string>('');
+	const [fordDistanceFactor, setFordDistanceFactor] = React.useState<string>('');
+	const [backdropScale, setBackdropScale] = React.useState<string>('');
+	const [sunAngleFactor, setSunAngleFactor] = React.useState<string>('');
+	const [resourceFactor, setResourceFactor] = React.useState<string>('');
+	const [deposits, setDeposits] = React.useState<string>('');
+	const [trees, setTrees] = React.useState<string>('');
+	const [seasons, setSeasons] = React.useState<string>('');
+	const [detail, setDetail] = React.useState<string>('');
+	const [prop, setProp] = React.useState<string>('');
 	
-	toTemplate (): string {
-		const {
-			noiseAmplitudes, distanceHeightOffset,
-			fordDistanceFactor, sunAngleFactor, backdropScale,
-			resourceFactor, deposits, trees,
-			seasons, detail, props,
-		} = this.state;
-		
-		const xml: string = [
-			'<?xml version="1.0" encoding="utf-8"?>',
-			'<environment>',
-			noiseAmplitudes,
-			resourceFactor,
-			distanceHeightOffset,
-			fordDistanceFactor,
-			backdropScale,
-			sunAngleFactor,
-			deposits,
-			detail,
-			props,
-			trees,
-			seasons,
-			'</environment>',
-		].join('');
+	/** Generate xml code */
+	const toTemplateText = (): string => {
+		const xml: string = `
+		<?xml version="1.0" encoding="utf-8"?>
+		<environment>
+			${noiseAmplitudes} ${resourceFactor}
+			${distanceHeightOffset} ${fordDistanceFactor}
+			${sunAngleFactor} ${backdropScale} ${deposits}
+			${detail} ${prop} ${trees} ${seasons}
+		</environment>`;
 		
 		return xmlFormatter(xml, {indentation: '  '});
 	}
 	
-	/**
-	 * @inheritDoc
-	 */
-	render (): Node {
-		return (
-			<>
-				<Accordion defaultActiveKey="environment_noise_amplitudes">
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2 position-relative">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_noise_amplitudes">
-								Noise Amplitudes
-							</Accordion.Toggle>
-							<span style={{
-								position: 'absolute', right: '1.2rem', top: '.8rem', fontSize: '.76rem',
-							}}>
-								<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2087224377"
-									title="This guide explains how the <noise_amplitude> element works in Environment configuration files. It will help build your intuition for how amplitudes influence one another so you can more quickly achieve the results you're looking for."
-									target="_blank" rel="noopener noreferrer">
-									Understanding Amplitudes in Environment
-								</a>
-							</span>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_noise_amplitudes">
-							<Card.Body>
-								<NoiseAmplitudes onChange={( v: string ) => this.setState({noiseAmplitudes: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain">
-								Terrain Features
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_terrain">
-							<Card.Body>
-								<ResourceFactor onChange={( v: string ) => this.setState({resourceFactor: v})}/>
-								<DistanceHeightOffset onChange={( v: string ) => this.setState({distanceHeightOffset: v})}/>
-								<FordDistanceFactor onChange={( v: string ) => this.setState({fordDistanceFactor: v})}/>
-								<SunAngleFactor onChange={( v: string ) => this.setState({sunAngleFactor: v})}/>
-								<BackdropScale onChange={( v: string ) => this.setState({backdropScale: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_deposit">
-								Deposit (Terrain)
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_terrain_deposit">
-							<Card.Body>
-								<DepositPanel onChange={( v: string ) => this.setState({deposits: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_detail">
-								Detail (Terrain)
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_terrain_detail">
-							<Card.Body>
-								<DetailPanel onChange={( v: string ) => this.setState({detail: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_props">
-								Props (Terrain)
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_terrain_props">
-							<Card.Body>
-								<PropPanel onChange={( v: string ) => this.setState({props: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_trees">
-								Trees (Terrain)
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_terrain_trees">
-							<Card.Body>
-								<TreesPanel onChange={( v: string ) => this.setState({trees: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-					<Card>
-						<Card.Header className="pt-1 pb-1 pl-2 pl-2">
-							<Accordion.Toggle as={Button} variant="link" eventKey="environment_seasons">
-								Seasons
-							</Accordion.Toggle>
-						</Card.Header>
-						<Accordion.Collapse eventKey="environment_seasons">
-							<Card.Body>
-								<Seasons onChange={( v: string ) => this.setState({seasons: v})}/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-				</Accordion>
-				<hr/>
-				<SyntaxHighlighter style={githubGist} language="xml">
-					{this.toTemplate()}
-				</SyntaxHighlighter>
-				<div className="mt-2">
-					<Button size="sm" variant="secondary"
-						onClick={() => copyClipboard(this.toTemplate())}>
-						Copy to Clipboard
-					</Button>
-				</div>
-			</>
-		);
-	}
+	return (
+		<>
+			<Accordion defaultActiveKey="environment_noise_amplitudes">
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2 position-relative">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_noise_amplitudes">
+							Noise Amplitudes
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_noise_amplitudes">
+						<Card.Body>
+							<NoiseAmplitudes onChange={v => setNoiseAmplitudes(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain">
+							Terrain Features
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_terrain">
+						<Card.Body>
+							<ResourceFactor onChange={v => setResourceFactor(v)}/>
+							<DistanceHeightOffset onChange={v => setDistanceHeightOffset(v)}/>
+							<FordDistanceFactor onChange={v => setFordDistanceFactor(v)}/>
+							<SunAngleFactor onChange={v => setSunAngleFactor(v)}/>
+							<BackdropScale onChange={v => setBackdropScale(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_deposit">
+							Deposit (Terrain)
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_terrain_deposit">
+						<Card.Body>
+							<DepositPanel onChange={v => setDeposits(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_detail">
+							Detail (Terrain)
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_terrain_detail">
+						<Card.Body>
+							<DetailPanel onChange={v => setDetail(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_props">
+							Props (Terrain)
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_terrain_props">
+						<Card.Body>
+							<PropPanel onChange={v => setProp(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_terrain_trees">
+							Trees (Terrain)
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_terrain_trees">
+						<Card.Body>
+							<TreesPanel onChange={v => setTrees(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+				<Card>
+					<Card.Header className="pt-1 pb-1 pl-2 pl-2">
+						<Accordion.Toggle as={Button} variant="link" eventKey="environment_seasons">
+							Seasons
+						</Accordion.Toggle>
+					</Card.Header>
+					<Accordion.Collapse eventKey="environment_seasons">
+						<Card.Body>
+							<SeasonsPanel onChange={v => setSeasons(v)}/>
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+			</Accordion>
+			<hr/>
+			<SyntaxHighlighter style={githubGist} language="xml">
+				{toTemplateText()}
+			</SyntaxHighlighter>
+			<div className="mt-2">
+				<Button size="sm" variant="secondary"
+					onClick={() => copyClipboard(toTemplateText())}>
+					Copy to Clipboard
+				</Button>
+			</div>
+		</>
+	);
 }
 
 export default EnvironmentContainer;
