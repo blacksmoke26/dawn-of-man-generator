ddde31d2c1af5b720a301dd6bb9817c6f0d8026d
Junaid Atari <mj.atari@gmail.com>
2020-08-27 19:16:28 +0500

    some clean up, add missing docs

--- src/panels/location/LocationContainer.js
+++ src/panels/location/LocationContainer.js
@@ -1,31 +1,25 @@
 // @flow
 
-import React, { useState, useCallback, useEffect } from 'react';
-import * as PropTypes from 'prop-types';
+import React from 'react';
 import { ButtonGroup, Button, Tabs, Tab, Card } from 'react-bootstrap';
 import { customAlphabet } from 'nanoid';
-
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { githubGist } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import xmlFormatter from 'xml-formatter';
 import copyClipboard from 'clipboard-copy';
 
-// Components
-import Locations from './generators/Locations';
-
 // Types
 import type { Node } from 'react';
 
+// Components
+import Locations from './generators/Locations';
+
 const randAlpha: Function = customAlphabet('abcdefghijklmnopqrstuvwxyz', 5);
 
-const LOCATIONS_MAX_COUNT: number = 9;
+/** Maximum limit of locations tabs */
+const LOCATIONS_MAX_COUNT: number = 12;
 
-/**
- * LocationContainer `props` type
- * @type {Object}
- */
-type Props = {};
-
+/** Tab contents wrapper */
 const TabContentWrapper = ( props: Object ): Node => {
 	return (
 		<div style={{marginTop: '1rem'}}>
@@ -38,50 +32,53 @@ const TabContentWrapper = ( props: Object ): Node => {
 	);
 };
 
-/**
- * LocationContainer functional component
- */
-const LocationContainer = ( props: Props ): Node => {
-	const [locations, setLocations] = useState({});
-	const [init, setInit] = useState(false);
-	const [activeKey, setActiveKey] = useState('');
-	const [loading, setLoading] = useState(false);
+/** LocationContainer functional component */
+function LocationContainer (): Node {
+	const [locations, setLocations] = React.useState<Object>({});
+	const [init, setInit] = React.useState<boolean>(false);
+	const [activeKey, setActiveKey] = React.useState<string>('');
+	const [loading, setLoading] = React.useState<boolean>(false);
 	
-	const addNewLocation = useCallback(() => {
+	React.useEffect(() => {
+		if ( !init ) {
+			setInit(true);
+			addNewLocation();
+		}
+	}, [init]);
+	
+	/** Add new location */
+	const addNewLocation = (): void => {
 		const id: string = randAlpha();
-		setLocations({...locations, [id]: ''});
+		setLocations(current => ({...current, [id]: ''}));
 		setActiveKey(id);
-	}, [locations, setLocations, setActiveKey]);
+	};
 	
-	useEffect(() => {
-		if ( !init ) { addNewLocation(); setInit(true); }
-	}, [addNewLocation, init, setInit]);
-	
+	/** Generate xml code */
 	const toTemplate: () => string = (): string => {
-		const xml: string = [
-			`<locations>`,
-			Object.values(locations).join(''),
-			`</locations>`
-		].join(``);
+		const xml: string = `
+		<locations>
+			${Object.values(locations).join('')}
+		</locations>`;
 		
 		return xmlFormatter(xml, {indentation: '  '});
 	};
 	
-	const totalLoc: number = Object.keys(locations).length;
+	/** Total locations count */
+	const total: number = Object.keys(locations).length;
 	
 	return (
 		<>
 			<div className="mb-3">
 				<ButtonGroup>
 					<Button variant="secondary" size="sm"
-						disabled={loading || totalLoc >= LOCATIONS_MAX_COUNT} onClick={() => {
-						if ( totalLoc <= LOCATIONS_MAX_COUNT  ) {
+						disabled={loading || total >= LOCATIONS_MAX_COUNT} onClick={() => {
+						if ( total <= LOCATIONS_MAX_COUNT  ) {
 							addNewLocation();
 							setLoading(true);
 							setTimeout(() => setLoading(false), 500);
 						}
 					}}>Add New</Button>
-					<Button variant="danger" size="sm" disabled={loading || totalLoc <= 1 } onClick={() => {
+					<Button variant="danger" size="sm" disabled={loading || total <= 1 } onClick={() => {
 						const key = Object.keys(locations)[0];
 						setLocations({[key]: locations[key]});
 						setActiveKey(key);
@@ -90,38 +87,35 @@ const LocationContainer = ( props: Props ): Node => {
 					}}>Remove All</Button>
 				</ButtonGroup>
 			</div>
-			
 			<Tabs activeKey={activeKey} id="locations-tab" onSelect={k => setActiveKey(k)}>
-				{Object.keys(locations).map((v, i: number) => {
-					return (
-						<Tab eventKey={v} key={v} as={'div'}
-							title={
-								<>
-									<span className="text-size-sm pr-2">{v}</span>
-									<Button hidden={i === 0} variant="link" className="text-color-default text-decoration-none p-0" style={{
-										lineHeight: '10px',
-										position: 'relative',
-										top: '-2px',
-									}} onClick={e => {
-										e.preventDefault();
-										e.stopPropagation();
-										const newLocations = {...locations};
-										const ky: Array<string> = Object.keys(locations);
-										const idx: number = Number(ky.findIndex(val => v === val));
-										delete newLocations[v];
-										setLocations(newLocations);
-										setActiveKey(ky[idx-1]);
-									}}>&times;</Button>
-								</>
-							}>
-							<TabContentWrapper>
-								<Locations onChange={( tpl, values) => {
-									setLocations({ ...locations, [v]: tpl});
-								}} />
-							</TabContentWrapper>
-						</Tab>
-					);
-				})}
+				{Object.keys(locations).map((v, i: number) => (
+					<Tab eventKey={v} key={v} as={'div'}
+						title={
+							<>
+								<span className="text-size-sm pr-2">#{++i}</span>
+								<Button hidden={i === 0} variant="link" className="text-color-default text-decoration-none p-0" style={{
+									lineHeight: '10px',
+									position: 'relative',
+									top: '-2px',
+								}} onClick={e => {
+									e.preventDefault();
+									e.stopPropagation();
+									const newLocations = {...locations};
+									const ky: Array<string> = Object.keys(locations);
+									const idx: number = Number(ky.findIndex(val => v === val));
+									delete newLocations[v];
+									setLocations(newLocations);
+									setActiveKey(ky[idx-1]);
+								}}>&times;</Button>
+							</>
+						}>
+						<TabContentWrapper>
+							<Locations onChange={( tpl, values) => {
+								setLocations({ ...locations, [v]: tpl});
+							}} />
+						</TabContentWrapper>
+					</Tab>
+				))}
 			</Tabs>
 			<hr/>
 			<SyntaxHighlighter style={githubGist} language="xml">
@@ -135,13 +129,6 @@ const LocationContainer = ( props: Props ): Node => {
 			</div>
 		</>
 	);
-};
-
-LocationContainer.defaultProps = {
-};
-
-LocationContainer.propTypes = {
-	onChange: PropTypes.func,
-};
+}
 
 export default LocationContainer;
