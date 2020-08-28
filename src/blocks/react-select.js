41d6cc4eab0c00c5fa7b5c784d3dc9373d6e6467
Junaid Atari <mj.atari@gmail.com>
2020-08-28 19:26:48 +0500

    create file

---
+++ src/blocks/react-select.js
@@ -0,0 +1,34 @@
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @link http://junaidatari.com Author Website
+ * @since 2020-08-28
+ */
+
+export const theme = (theme) => ({
+	...theme,
+	borderRadius: 0,
+	colors: {
+		...theme.colors,
+		danger: '#DE350B',
+		dangerLight: '#FFBDAD',
+		neutral0: '#2a3141',
+		neutral10: '#2a3141',
+		neutral20: '#293040',
+		neutral30: '#293040',
+		neutral40: 'rgba(255,255,255,.1)',
+		neutral50: 'rgba(255,255,255,.2)',
+		neutral60: 'rgba(255,255,255,.7)',
+		neutral70: 'rgba(255,255,255,.7)',
+		neutral80: 'rgba(255,255,255,.5)',
+		neutral90: 'rgba(255,255,255,.1)',
+		primary: '#2a3141',
+		primary25: '#363847',
+		primary50: '#33343e',
+		primary75: '#37373e',
+	},
+});
+
+export const styles = {
+	menuPortal: base => ({...base, zIndex: 9999}),
+	control: styles => ({ ...styles, backgroundColor: 'rgb(242 242 242 / 4%)' }),
+};
