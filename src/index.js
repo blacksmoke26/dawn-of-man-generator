6c931a015ff1ae6cc3b204e3821ff67b8556595e
Junaid Atari <mj.atari@gmail.com>
2020-08-28 19:26:28 +0500

    import styles files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/uikit/bootstrap.min.css';
+import './styles/uikit/bootstrap_limitless.min.css';
+import './styles/uikit/layout.min.css';
+import './styles/uikit/colors.min.css';
 import 'rc-slider/assets/index.css';
+import './styles/custom.css';
 import './styles/global.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
