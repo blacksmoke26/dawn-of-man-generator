8c9f72156e5fc4f9ab0766767812c94cc0da35b6
Junaid Atari <mj.atari@gmail.com>
2020-08-29 17:59:42 +0500

    create file

---
+++ src/redux/action-types.js
@@ -0,0 +1,9 @@
+// @flow
+
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @link http://junaidatari.com Author Website
+ * @since 2020-08-29
+ */
+
+export const ENVIRONMENT_NOISE_AMPLITUDES: string = 'ENVIRONMENT_NOISE_AMPLITUDES';
---
+++ src/redux/actions.js
@@ -0,0 +1,8 @@
+// @flow
+
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @link http://junaidatari.com Author Website
+ * @since 2020-08-29
+ */
+ 
