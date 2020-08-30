8e9edf05aea6ad88db1159ea67be0a2199c37db0
Junaid Atari <mj.atari@gmail.com>
2020-08-30 12:12:22 +0500

    create file

---
+++ src/data/environments/builtin/index.js
@@ -0,0 +1,23 @@
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @link http://junaidatari.com Author Website
+ * @since 2020-08-29
+ */
+
+import eurasia from './eurasia';
+import eurasia_conflict from './eurasia_conflict';
+import eurasia_flatlands from './eurasia_flatlands';
+import eurasia_glacial from './eurasia_glacial';
+import eurasia_north from './eurasia_north';
+import eurasia_warm from './eurasia_warm';
+import flat from './flat';
+
+export {
+	eurasia,
+	eurasia_conflict,
+	eurasia_flatlands,
+	eurasia_glacial,
+	eurasia_north,
+	eurasia_warm,
+	flat,
+}
