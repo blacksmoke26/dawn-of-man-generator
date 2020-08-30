85dbbc6c79739c18aad323f47af203163f662a42
Junaid Atari <mj.atari@gmail.com>
2020-08-30 13:29:20 +0500

    create redux action method

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,4 +5,24 @@
  * @link http://junaidatari.com Author Website
  * @since 2020-08-29
  */
- 
+
+// Redux actions
+import {
+	SET_ENVIRONMENT,
+} from './action-types';
+
+// Utils
+import { jsonToRedux } from './../data/environments/parser';
+
+/**
+ * @public
+ * @static
+ * @async
+ * Set environment data
+ */
+export function setEnvironment ( rawData: Object ): Promise<void> {
+	return async ( dispatch: Dispatch ) => {
+		const {environment}: Object = jsonToRedux(rawData);
+		dispatch({type: SET_ENVIRONMENT, payload: environment});
+	};
+}
