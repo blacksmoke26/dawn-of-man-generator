3442a37299569ce8345c30d7447de8ed84e02c1b
Junaid Atari <mj.atari@gmail.com>
2020-08-30 13:39:03 +0500

    create new constant

--- src/data/environments/builtin/index.js
+++ src/data/environments/builtin/index.js
@@ -4,15 +4,23 @@
  * @since 2020-08-29
  */
 
-import eurasia from './eurasia';
-import eurasia_conflict from './eurasia_conflict';
-import eurasia_flatlands from './eurasia_flatlands';
-import eurasia_glacial from './eurasia_glacial';
-import eurasia_north from './eurasia_north';
-import eurasia_warm from './eurasia_warm';
-import flat from './flat';
+import eurasia, { info as info1 } from './eurasia';
+import eurasia_conflict, { info as info2 } from './eurasia_conflict';
+import eurasia_flatlands, { info as info3 } from './eurasia_flatlands';
+import eurasia_glacial, { info as info4 } from './eurasia_glacial';
+import eurasia_north, { info as info5 } from './eurasia_north';
+import eurasia_warm, { info as info6 } from './eurasia_warm';
+import flat, { info as info7 } from './flat';
 
-export {
+/** Builtin environments labels */
+export const labels: Array<Object> = [
+	info1, info2, info3,
+	info4, info5, info6,
+	info7,
+];
+
+/** Builtin environments */
+export const environments: Object = {
 	eurasia,
 	eurasia_conflict,
 	eurasia_flatlands,
@@ -20,4 +28,4 @@ export {
 	eurasia_north,
 	eurasia_warm,
 	flat,
-}
+};
