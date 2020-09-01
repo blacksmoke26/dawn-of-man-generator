dae81a78d38c469b3f682598d2d3a351503716d0
Junaid Atari <mj.atari@gmail.com>
2020-09-01 15:00:12 +0500

    fix input focus, add new buttons, some ui changes

--- src/panels/environment/loaders/XmlTextLoader.js
+++ src/panels/environment/loaders/XmlTextLoader.js
@@ -8,11 +8,11 @@
 
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, Modal, Form, Alert } from 'react-bootstrap';
+import { ButtonGroup, Button, Modal, Form, Alert, Row, Col } from 'react-bootstrap';
 import xmlFormatter from 'xml-formatter';
 
 // Types
-import type { Node } from 'react';
+import type { Node, Ref } from 'react';
 import type { Dispatch } from 'redux';
 
 // Redux
@@ -21,17 +21,25 @@ import { setParsedEnvironment } from './../../../redux/actions';
 // Utils
 import { xmlToReduxJson, validateXml } from './../../../data/environments/loader/index';
 
-/**
- * XmlTextLoader functional component
- */
-const XmlTextLoader = (): Node => {
+/** XmlTextLoader functional component */
+function XmlTextLoader (): Node {
 	const dispatch: Dispatch = useDispatch();
+	const inputRef: Ref = React.createRef();
+	
 	const [showModel, setShowModel] = React.useState<boolean>(false);
 	const [error, setError] = React.useState<string>('');
 	const [value, setValue] = React.useState<string>('');
-	const isValidXml: boolean = validateXml(value);
 	
-	const onImportClick = async (): void => {
+	const xmlValidated = validateXml(value.trim());
+	const isXmlValid: boolean = value.trim() && xmlValidated.valid;
+	
+	/** Focus input field */
+	const focusInput = (): void => {
+		inputRef.current && inputRef.current.focus();
+	};
+	
+	/** Import button click handler */
+	const onImportClick = React.useCallback(async (): void => {
 		try {
 			const json = xmlToReduxJson(value);
 			await dispatch(setParsedEnvironment(json));
@@ -39,13 +47,31 @@ const XmlTextLoader = (): Node => {
 		} catch ( e ) {
 			setError(e.message);
 		}
-	};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [value]);
+	
+	/** Prettify button click handler */
+	const onPrettifyClick = React.useCallback((): void => {
+		setValue(
+			xmlFormatter(value, {indentation: '  '})
+		);
+		focusInput();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [value]);
+	
+	/** Clear button click handler */
+	const onClearClick = React.useCallback((): void => {
+		setValue('');
+		focusInput();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [value]);
 
 	return (
 		<>
 			<Button variant="light" onClick={() => setShowModel(true)}>Paste Text</Button>
 			<Modal size="xl"
 				show={showModel}
+				onShow={() => focusInput()}
 				onHide={() => setShowModel(false)}
 				backdrop="static"
 				keyboard={false}>
@@ -53,36 +79,46 @@ const XmlTextLoader = (): Node => {
 					<Modal.Title>Paste XML Text</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<Form.Control value={value} onChange={(e) => {
+					<Form.Control ref={inputRef} value={value} onChange={(e) => {
 						const val: string = String(e.currentTarget.value).trim();
 						setValue(val);
 						setTimeout(() => setError(''), 50);
 					}} as="textarea" className="font-family-code" style={{height: 500}} />
-					{error && (
-						<Alert className="mt-2 mb-0" variant="danger">
-							<strong>Error</strong>: {error}
+					{value.trim() && (error || xmlValidated.error) && (
+						<Alert className="mt-3 mb-0" variant="danger">
+							<strong>Error</strong>: {error || xmlValidated.error}
 						</Alert>
 					)}
 				</Modal.Body>
-				<Modal.Footer>
-					<Button variant=""
-						disabled={!isValidXml}
-						onClick={() => {
-							const formatted = xmlFormatter(value, {indentation: '  '})
-							setValue(formatted);
-						}}>Format XML</Button>
-					<Button variant="primary"
-						disabled={!isValidXml}
-						onClick={() => onImportClick()}>Import</Button>
-					<Button
-						variant="secondary"
-						onClick={() => setShowModel(false)}>
-						Discard
-					</Button>
+				<Modal.Footer className="d-block">
+					<Row>
+						<Col sm="6">
+							<ButtonGroup size="sm">
+								<Button variant=""
+									disabled={!isXmlValid}
+									onClick={() => onPrettifyClick()}>Prettify</Button>
+								<Button variant=""
+									disabled={!value.trim()}
+									onClick={() => onClearClick()}>Clear</Button>
+							</ButtonGroup>
+						</Col>
+						<Col sm="6" className="text-right">
+							<ButtonGroup size="sm">
+								<Button variant="success"
+									disabled={!isXmlValid}
+									onClick={() => onImportClick()}>Import</Button>
+								<Button
+									variant="secondary"
+									onClick={() => setShowModel(false)}>
+									Discard
+								</Button>
+							</ButtonGroup>
+						</Col>
+					</Row>
 				</Modal.Footer>
 			</Modal>
 		</>
 	);
-};
+}
 
 export default XmlTextLoader;
