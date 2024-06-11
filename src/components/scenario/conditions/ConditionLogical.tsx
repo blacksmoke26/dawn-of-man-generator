70b1a13997f052716abd6bcb3f05167e9896534a
Junaid Atari <mj.atari@gmail.com>
2024-06-11 15:13:21 +0500

    implement new props, top bar and functional changes

--- src/components/scenario/conditions/ConditionLogical.tsx
+++ src/components/scenario/conditions/ConditionLogical.tsx
@@ -9,17 +9,17 @@ import * as PropTypes from 'prop-types';
 import cn from 'classname';
 import merge from 'deepmerge';
 import {nanoid} from 'nanoid';
-import {Col, Form, Row} from 'react-bootstrap';
+import {Button, Col, Form, Row} from 'react-bootstrap';
 import {camelCase, capitalCase} from 'change-case';
 
 // types
 import type {Required} from 'utility-types';
 import type {
-  SubConditions,
-  LogicalCondition,
-  GeneralConditionKey,
-  GeneralCondition,
   ConditionType as ConditionTypeBase,
+  GeneralCondition,
+  GeneralConditionKey,
+  LogicalCondition,
+  SubConditions,
 } from '~/types/condition.types';
 
 // conditions sub-components
@@ -41,26 +41,40 @@ import ValueReached from './ValueReached';
 
 // icons
 import {
-  IconConditionOr,
+  COLOR_DISABLED,
+  COLOR_REDDISH,
+  IconChevronDown,
+  IconChevronUp,
+  IconCondition,
   IconConditionAnd,
   IconConditionNot,
-  IconCondition,
-  COLOR_DISABLED, COLOR_REDDISH
+  IconConditionOr,
+  IconSquareMinus
 } from '~/components/icons/app';
 
 // elemental components
 import Select, {Option} from '~/components/ui/Select';
 
 // utils
-import {GENERAL_CONDITIONS, LOGICAL_CONDITION, defaultsParams} from '~/utils/condition';
+import {defaultsParams, GENERAL_CONDITIONS, LOGICAL_CONDITION} from '~/utils/condition';
+
+export interface ChangedValues {
+  enabled: boolean,
+  subConditions: ConditionList;
+}
 
 /** ConditionNot `props` type */
 interface Props {
   enabled?: boolean,
+  expanded?: boolean,
   operator: LogicalCondition;
   subConditions?: SubConditions;
+  removeIcon?: boolean;
+  disabledCheckbox?: boolean;
 
-  onChange(template: string, conditions: ConditionList): void,
+  onRemoveClick?(): void,
+
+  onChange(template: string, values: ChangedValues): void,
 }
 
 const componentsMap = {
@@ -91,6 +105,7 @@ const conditionIcon = (operator: LogicalCondition) => {
 
 export type ConditionType = Required<ConditionTypeBase, 'internalName'> & {
   enabled?: boolean,
+  expanded?: boolean,
   template?: string,
 };
 
@@ -108,16 +123,24 @@ const createConditionComponent = (condition: ConditionType): React.FC<any> => {
 const ConditionLogical = (props: Props) => {
   const newProps = merge({
     enabled: true,
+    expanded: true,
     subConditions: [],
+    removeIcon: false,
+    onRemoveClick: () => {
+    },
     onChange: () => {
     },
   }, props);
 
   const [enabled, setEnabled] = React.useState<boolean>(newProps.enabled as boolean);
+  const [disabledCheckbox, setDisabledCheckbox] = React.useState<boolean>(newProps.disabledCheckbox as boolean);
+  const [expanded, setExpanded] = React.useState<boolean>(newProps.expanded as boolean);
   const [subConditions, setSubConditions] = React.useState<ConditionList>({});
 
   const triggerHandler = () => {
-    typeof props.onChange === 'function' && props.onChange(toTemplateText(), subConditions);
+    typeof props.onChange === 'function' && props.onChange(toTemplateText(), {
+      enabled, subConditions,
+    });
   };
 
   /** Generate xml code */
@@ -140,14 +163,46 @@ const ConditionLogical = (props: Props) => {
     });
   };
 
+  const removeSubCondition = (id: string): void => {
+    setSubConditions(current => {
+      const _current = {...current};
+      delete _current[id];
+      return _current;
+    });
+  };
+
+  const setSubConditionsProperty = <V = any>(prop: string, value: V): void => {
+    setSubConditions(current => {
+      const _current = {...current};
+      Object.keys(_current).forEach(key => {
+        const condition: { [p: string]: any } = {...current[key]};
+        if (prop in condition) {
+          condition[prop as string] = value;
+        }
+        _current[key] = condition as ConditionType;
+      });
+      return _current;
+    });
+  };
+
+  // Reflect prop changes
+  React.useEffect(() => {
+    props?.enabled !== undefined && setEnabled(Boolean(props?.enabled));
+    props?.disabledCheckbox !== undefined && setDisabledCheckbox(Boolean(props?.disabledCheckbox));
+    props?.expanded !== undefined && setExpanded(Boolean(props?.expanded));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props]);
+
   const subConditionsList = Object.entries(subConditions);
   const totalCount = subConditionsList.length;
+
+  const isDisabled = disabledCheckbox || !enabled || !Object.keys(subConditions).length;
 
   return (
     <div className={cn({'text-muted': !enabled})}>
       <div style={{background: '#333', padding: '11px 11px 2px 11px', borderRadius: 2}}>
         <Row>
-          <Col col="3" className="checkbox-align">
+          <Col col="6" className="checkbox-align">
             <Form.Check
               type="switch"
               id={`locations_override-switch-${nanoid(5)}`}
@@ -159,76 +214,146 @@ const ConditionLogical = (props: Props) => {
             </span>
             </span>}
               checked={enabled}
+              disabled={disabledCheckbox}
               onChange={e => setEnabled(e.target.checked)}
             />
           </Col>
-          <Col col="3" className="text-right" style={{top: -4}}>
-            <strong style={{color: COLOR_REDDISH}}
-                    className={cn('text-size-sm', {'text-muted': !enabled})}>
-              {conditionIcon(newProps.operator)}
-              {' '}
-              <span className="position-relative" style={{top: '0.01rem'}}>
+          <Col col="6" className="text-right" style={{top: -4}}>
+            <div className="d-inline-block">
+              <strong style={{color: '#ebeaea'}}
+                      className={cn('text-size-sm', {'text-muted': !enabled})}>
+                {conditionIcon(newProps.operator)}
+                {' '}
+                <span className="position-relative" style={{top: '0.01rem'}}>
                 {newProps.operator?.toUpperCase()}
               </span>
-            </strong>
+              </strong>
+            </div>
+            {newProps?.removeIcon && (
+              <div className="d-inline-block">
+                <Button variant="link" className="p-0 ml-2 mr-1"
+                        style={{
+                          top: '-0.01rem',
+                          color: COLOR_REDDISH
+                        }}
+                        title="Remove condition"
+                        onClick={() => {
+                          'function' === typeof newProps?.onRemoveClick && newProps?.onRemoveClick();
+                        }}>
+                  <IconSquareMinus width="16" height="16"/>
+                </Button>
+              </div>
+            )}
+            <div className="d-inline-block">
+              <Button variant="link" className="p-0" style={{top: '-0.062rem'}}
+                      disabled={disabledCheckbox}
+                      onClick={() => {
+                        setExpanded(!expanded);
+                      }}>
+                {!expanded
+                  ? <IconChevronUp width="16" height="16"/>
+                  : <IconChevronDown width="16" height="16"/>}
+              </Button>
+            </div>
           </Col>
         </Row>
       </div>
-      <div className="mt-2 mb-2 pl-3">
-        <div className="d-inline-block mr-2">
-          Pick condition:
-        </div>
-        <div className="d-inline-block mr-2" style={{width: '40%'}}>
-          <Select
-            isDisabled={!enabled}
-            menuPortalTarget={document.body}
-            value={null}
-            formatOptionLabel={(option: Option | any) => (
-              <span>
+      {expanded && (
+        <>
+          <div className="mt-2 mb-2 pl-3">
+            <Row className={cn({'text-muted': false}, 'checkbox-align')}>
+              <Col xs="8">
+                <div className="d-inline-block" style={{width: '80%'}}>
+                  <Select
+                    isDisabled={!enabled}
+                    menuPortalTarget={document.body}
+                    value={null}
+                    formatOptionLabel={(option: Option | any) => (
+                      <span>
                   <IconCondition width="17" height="17" color={COLOR_REDDISH}/>
-                {' '} {option?.label}</span>
-            )}
-            options={GENERAL_CONDITIONS.map(value => ({label: capitalCase(value), value}))}
-            placeholder="Insert condition..."
-            onChange={(option: Option | any, {action}): void => {
-              if (action === 'select-option' && option) {
-                const defaultConfigKey = camelCase(option?.value) as GeneralConditionKey;
-                const newCondition = merge<ConditionType>({
-                    internalName: option.value as GeneralCondition,
-                    enabled: true,
-                    template: '',
-                  },
-                  defaultsParams?.[defaultConfigKey] || {}
-                );
-                setSubConditions(current => ({...current, [nanoid(10)]: newCondition}));
-              }
-            }}
-          />
-        </div>
-        <div className="clearfix"></div>
-      </div>
-      <div className="mt-3">
-        {subConditionsList.map(([_id, condition], index) => {
-          const ConditionComponent = createConditionComponent(condition);
-          const componentProps = {...condition} as Partial<ConditionType>;
-          delete componentProps?.internalName;
+                        {' '} {option?.label}</span>
+                    )}
+                    options={GENERAL_CONDITIONS.map(value => ({label: capitalCase(value), value}))}
+                    placeholder="Choose condition to insert..."
+                    onChange={(option: Option | any, {action}): void => {
+                      if (action === 'select-option' && option) {
+                        const defaultConfigKey = camelCase(option?.value) as GeneralConditionKey;
+                        const newCondition = merge<ConditionType>({
+                            internalName: option.value as GeneralCondition,
+                            enabled: true,
+                            expanded: true,
+                            template: '',
+                          },
+                          defaultsParams?.[defaultConfigKey] || {}
+                        );
+                        setSubConditions(current => ({...current, [nanoid(10)]: newCondition}));
+                      }
+                    }}
+                  />
+                </div>
+              </Col>
+              <Col xs="4" className="text-right">
+                <div className="pt-1">
+                  <div className="d-inline-block">
+                    <Button variant="link" className="p-0"
+                            title="Collapse all conditions"
+                            style={{top: '-0.2rem', marginRight: '0.1rem'}}
+                            disabled={isDisabled}
+                            onClick={() => setSubConditionsProperty('expanded', false)}>
+                      <IconChevronUp width="16" height="16"/>
+                    </Button>
+                  </div>
+                  <div className="d-inline-block ml-1">
+                    <Button variant="link" className="p-0"
+                            title="Expand all conditions"
+                            style={{top: '-0.2rem', marginRight: '0.05rem'}}
+                            disabled={isDisabled}
+                            onClick={() => setSubConditionsProperty('expanded', true)}>
+                      <IconChevronDown width="16" height="16"/>
+                    </Button>
+                  </div>
+                  <div className="d-inline-block">
+                    <Button variant="link" className="p-0 ml-1"
+                            style={{
+                              top: '-0.20rem',
+                              color: isDisabled ? 'rgba(255, 255, 255, .4)' : COLOR_REDDISH
+                            }}
+                            title="Remove all sub conditions"
+                            disabled={isDisabled}
+                            onClick={() => setSubConditions({})}>
+                      <IconSquareMinus width="16" height="16"/>
+                    </Button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="mt-3">
+            {subConditionsList.map(([_id, condition], index) => {
+              const ConditionComponent = createConditionComponent(condition);
+              const componentProps = {...condition} as Partial<ConditionType>;
+              delete componentProps?.internalName;
 
-          return (
-            <React.Fragment key={_id}>
-              <div className="ml-3 pl-3" style={{borderLeft: '2px solid #616978'}}>
-                <ConditionComponent
-                  {...componentProps}
-                  disabledCheckbox={!enabled}
-                  onChange={(template: string, values: Record<string, any>) => {
-                    updateSubCondition(_id, {...values, template});
-                    triggerHandler();
-                  }}/>
-              </div>
-              {index < totalCount-1 && <hr className="mt-2 mb-2"/>}
-            </React.Fragment>
-          );
-        })}
-      </div>
+              return (
+                <React.Fragment key={_id}>
+                  <div className="ml-3 pl-3" style={{borderLeft: '2px solid #616978'}}>
+                    <ConditionComponent
+                      {...componentProps}
+                      disabledCheckbox={!enabled}
+                      removeIcon={true}
+                      onRemoveClick={() => removeSubCondition(_id)}
+                      onChange={(template: string, values: Record<string, any>) => {
+                        updateSubCondition(_id, {...values, template});
+                        triggerHandler();
+                      }}/>
+                  </div>
+                  {index < totalCount - 1 && <hr className="mt-2 mb-2"/>}
+                </React.Fragment>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
