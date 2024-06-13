e30f02e4e6833785a195d3be8a3cf434c3c175ed
utf4 <furqan.dev02@invomail.io>
2024-06-13 16:59:12 +0500

    create component

--- src/panel/scenario/generators/milestones/Milestone.tsx
+++ src/panel/scenario/generators/milestones/Milestone.tsx
@@ -6,130 +6,115 @@
 
 import React from 'react';
 import * as PropTypes from 'prop-types';
+import cn from 'classname';
 import merge from 'deepmerge';
+import {nanoid} from 'nanoid';
+import xmlFormatter from 'xml-formatter';
+import {Col, Form, Row} from 'react-bootstrap';
+
+// elemental components
+import Select, {Option} from '~/components/ui/Select';
+
+// utils
+import {toString} from '~/helpers/string';
+import {onlyKeys} from '~/helpers/object';
+import {CONDITIONS_OPTIONS, LOGICAL_CONDITION} from '~/utils/condition';
+
+// components
+import {Condition, ConditionsState, ConditionRegistry} from './Condition';
 
 // types
-import {Col, Form, Row} from 'react-bootstrap';
-import {nanoid} from 'nanoid';
-import Select, {Option} from '~/components/ui/Select';
-import {Milestone as MilestoneType} from '~/types/milestone.types';
-import {CONDITIONS_OPTIONS, LOGICAL_CONDITION} from '~/utils/condition';
-import {toString} from '~/helpers/string';
+import type {Required} from 'utility-types';
+import type {Milestone as MilestoneType} from '~/types/milestone.types';
+import type {GeneralCondition, LogicalCondition} from '~/types/condition.types';
 
-import AnyTasksActive from '~/components/scenario/conditions/AnyTasksActive';
-import AnyWorkAreasActive from '~/components/scenario/conditions/AnyWorkAreasActive';
-import EntityCountComparison from '~/components/scenario/conditions/EntityCountComparison';
-import EntityCountReached from '~/components/scenario/conditions/EntityCountReached';
-import EntityNearMarker from '~/components/scenario/conditions/EntityNearMarker';
-import EraUnlocked from '~/components/scenario/conditions/EraUnlocked';
-import InitGame from '~/components/scenario/conditions/InitGame';
-import IsAlive from '~/components/scenario/conditions/IsAlive';
-import IsGameInteractionPending from '~/components/scenario/conditions/IsGameInteractionPending';
-import NewGame from '~/components/scenario/conditions/NewGame';
-import ScenarioCompleted from '~/components/scenario/conditions/ScenarioCompleted';
-import TechUnlocked from '~/components/scenario/conditions/TechUnlocked';
-import TimeElapsed from '~/components/scenario/conditions/TimeElapsed';
-import ValueEquals from '~/components/scenario/conditions/ValueEquals';
-import ValueReached from '~/components/scenario/conditions/ValueReached';
-import ConditionLogical from '~/components/scenario/conditions/ConditionLogical';
+const isEqual = require('is-equal');
+
+type KVDocument = { [key: string]: any };
 
 export interface ChangedValues {
+  [key: string]:
+    { type: GeneralCondition } & KVDocument
+    | {
+    type: LogicalCondition,
+    subConditions: { type: GeneralCondition } & KVDocument
+  } & KVDocument;
 }
 
 export interface Props {
   id?: string;
+  disabled?: boolean;
   conditions?: MilestoneType['conditions'];
 
   onChange?(template: string, values: ChangedValues): void,
 }
 
-interface ConditionRegistry {
-  [uniqueName: string]: any;
-
-  enabled: boolean,
-  disabledCheckbox: boolean,
-  expanded: boolean,
-  template: string,
-  internalName: string;
-  subConditions?: ConditionsState;
-}
-
-interface ConditionsState {
-  [uniqueName: string]: ConditionRegistry;
-}
-
-const componentsMap: Record<string, React.FC<any>> = {
-  ConditionLogical,
-  AnyTasksActive,
-  AnyWorkAreasActive,
-  EntityCountComparison,
-  EntityCountReached,
-  EntityNearMarker,
-  EraUnlocked,
-  InitGame,
-  IsAlive,
-  IsGameInteractionPending,
-  NewGame,
-  ScenarioCompleted,
-  TechUnlocked,
-  TimeElapsed,
-  ValueEquals,
-  ValueReached,
+const omitProps = <T extends KVDocument = KVDocument>(props: T): T => {
+  return onlyKeys(props, ['expanded'], true) as T;
 };
 
-const createConditionComponent = (condition: ConditionRegistry): React.FC<any> => {
-  let name = condition.internalName as string;
-  if (LOGICAL_CONDITION.includes(name)) {
-    name = 'ConditionLogical';
-  }
-  if (!(name in componentsMap)) {
-    throw new Error(`Unknown condition component: ${name}`);
+const conditionsToValues = (conditions: KVDocument): ChangedValues => {
+  const registry: ChangedValues = {};
+  let index = 0;
+
+  for (const [, condition] of Object.entries(conditions)) {
+    index++;
+    registry[`condition_${index}`] = onlyKeys(condition, ['expanded', 'disabledCheckbox', 'enabled', 'template'], true);
   }
 
-  return componentsMap[name];
+  return registry;
 };
 
+const toTemplateText = (milestoneId: string, attributes: KVDocument): string => {
+  return xmlFormatter(
+    `<milestone id="${milestoneId.trim() || 'untitled'}">\n\t`
+    + Object.values(attributes).map((attr => attr.template)).join('\t\n')
+    + '</milestone>',
+  );
+};
+
+/** Milestone functional component */
 const Milestone = (props: Props) => {
-  const newProps = merge<Props>({
+  const newProps = merge<Required<Props>>({
     id: '',
     conditions: {},
+    disabled: false,
     onChange: () => {
     },
   }, props);
 
   const [milestoneId, setMilestoneId] = React.useState<string>(toString(newProps.id));
-  const [conditions, setConditions] = React.useState<ConditionsState>({});
-  const [disabled, setDisabled] = React.useState<boolean>(true);
+  const [conditions, setConditions] = React.useState<ConditionsState>(newProps?.conditions as any);
+  const [disabled, setDisabled] = React.useState<boolean>(newProps.disabled);
+  const [attributes, setAttributes] = React.useState<KVDocument>({});
 
   const updateSubCondition = (id: string, values: Record<string, any> = {}): void => {
     setConditions((current) => {
-      /*current[id] = merge(current?.[id] || {}, values as Partial<ConditionsState>);
-      return current;*/
       return {...current, [id]: merge(current?.[id] || {}, values as Partial<ConditionsState>)};
     });
   };
 
-  const toTemplateText = (): string => {
-    /*return !attributes.enabled || !String(attributes?.workAreaId || '')?.trim()
-      ? ''
-      : (
-        `<milestone type="${CONDITION_NAME}"`
-        + ` work_area_id="${attributes?.workAreaId}"`
-        + ` max_workers="${attributes.maxWorkers}"/>`
-      );*/
-    return '';
+  const removeCondition = (id: string): void => {
+    setConditions(current => {
+      const _current = {...current};
+      delete _current[id];
+      return _current;
+    });
   };
 
   // Reflect state changes
   React.useEffect(() => {
-    typeof newProps.onChange === 'function' && newProps.onChange(toTemplateText(), conditions);
+    const template = disabled ? '' : toTemplateText(milestoneId, attributes);
+    typeof newProps.onChange === 'function' && newProps.onChange(template, disabled ? {} : conditionsToValues(attributes));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [conditions]);
+  }, [disabled, milestoneId, attributes]);
 
   // Reflect prop changes
   React.useEffect(() => {
     //props?.id && setDisabled(props.);
-    props?.id && setMilestoneId(props?.id);
+    props?.id !== undefined && setMilestoneId(props.id);
+    props?.conditions !== undefined && setConditions(props.conditions as any);
+    props?.disabled !== undefined && setDisabled(props.disabled);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props]);
 
@@ -141,37 +126,48 @@ const Milestone = (props: Props) => {
     let index = 0;
 
     for (const [_id, condition] of Object.entries(conditions)) {
-      console.log('condition:', condition);
-      const ConditionComponent = createConditionComponent(condition);
-      const componentProps = {...condition} as Record<string, any>;
-
-      delete componentProps?.internalName;
+      const values = {
+        ...condition,
+        enabled: !disabled,
+        disabledCheckbox: disabled,
+      } as ConditionRegistry;
 
       nodes.push(
         <React.Fragment key={_id}>
-          <div className="ml-3 pl-3" style={{borderLeft: '2px solid #616978'}}>
-            <ConditionComponent
-              {...componentProps}
-              disabledCheckbox={disabled}
-              removeIcon={true}
-              /*onRemoveClick={() => removeSubCondition(_id)}*/
-              onChange={(template: string, values: Record<string, any>) => {
-                console.log('values', values)
-                //updateSubCondition(_id, {...values, template});
-                //triggerHandler();
-              }}/>
+          <div className="ml-2 pl-3" style={{borderLeft: '2px solid #616978'}}>
+            <Condition id={_id} internalName={condition.internalName}
+                       values={values}
+                       disabledCheckbox={disabled}
+                       onChange={(template: string, values) => {
+                         const nextState = {...omitProps(values), template};
+                         setAttributes(prevState => {
+                           if (isEqual(prevState[_id], nextState)) {
+                             return prevState as ChangedValues;
+                           }
+                           return {...prevState, [_id]: nextState} as ChangedValues;
+                         });
+                       }}
+                       onRemoveClick={() => {
+                         removeCondition(_id);
+                         setAttributes(prevState => {
+                           const current = {...prevState};
+                           delete current[_id];
+                           return current;
+                         });
+                       }}
+            />
           </div>
-          {index < totalCount - 1 && <hr className="mt-2 mb-2"/>}
-        </React.Fragment>
+          {index < totalCount - 1 && <hr className="mt-2 mb-3"/>}
+        </React.Fragment>,
       );
       index++;
     }
 
     return nodes;
-  }
+  };
 
   return (
-    <div className="mt-0 checkbox-align ">
+    <div className={cn('mt-0 checkbox-align', {'text-muted': disabled})}>
       <Form.Check
         className="pull-right"
         type="switch"
@@ -192,12 +188,14 @@ const Milestone = (props: Props) => {
             className="pull-right"
             aria-disabled={disabled}
             id={`condition-${nanoid(5)}`}
-            placeholder="e.g., first_time"
+            placeholder="e.g., survival_5_weeks"
             value={milestoneId}
-            onChange={e => setMilestoneId(e.target.value.replace(/['"]+/ig, ``))}
+            onChange={e => {
+              setMilestoneId(e.target.value.replace(/(['" \t]|[^a-z_\d])+/ig, `_`));
+            }}
             onKeyUp={e => {
               // @ts-ignore
-              e.target.value = e.target.value.replace(/['"]+/ig, ``).replace(/\t+/ig, `_`).toLowerCase();
+              e.target.value = e.target.value.replace(/(['" \t]|[^a-z_\d])+/ig, `_`).toLowerCase();
             }}
           />
         </Col>
@@ -211,19 +209,25 @@ const Milestone = (props: Props) => {
           placeholder="Add milestone condition..."
           onChange={(option: Option | any, {action}): void => {
             if (action === 'select-option' && option) {
-              const values: ConditionRegistry = {
+              const conditionId: string = nanoid(10).toLowerCase();
+              const values = {
                 internalName: option.value as string,
                 disabledCheckbox: false,
                 enabled: true,
                 expanded: true,
                 template: '',
-              };
+              } as ConditionRegistry;
 
               if (LOGICAL_CONDITION.includes(option.value)) {
                 values['subConditions'] = {};
                 values['operator'] = option.value;
               }
-              updateSubCondition(nanoid(10).toLowerCase(), values);
+              updateSubCondition(conditionId, values);
+              setAttributes(current => ({
+                ...current, [conditionId]: {
+                  template: '',
+                },
+              }));
             }
           }}
         />
@@ -236,6 +240,8 @@ const Milestone = (props: Props) => {
 // Properties validation
 Milestone.propTypes = {
   id: PropTypes.string,
+  disabled: PropTypes.bool,
+  conditions: PropTypes.object,
 };
 
 export default Milestone;
