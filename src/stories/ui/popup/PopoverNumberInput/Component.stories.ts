653bdb1fed6746dd8b0da5831c76ab9f5fdfae66
Junaid Atari <mj.atari@gmail.com>
2024-06-30 23:31:34 +0500

    create generic component `PopoverNumberInput` and storybook stories

---
+++ src/stories/ui/popup/PopoverNumberInput/Component.stories.ts
@@ -0,0 +1,45 @@
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @see https://github.com/blacksmoke26/dawn-of-man-generator
+ * @since 2024-06-30
+ * @version 2.5
+ */
+
+import {fn} from '@storybook/test';
+
+// components
+import {Component} from './Component';
+
+// types
+import type {Meta, StoryObj} from '@storybook/react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'UI/Popup/PopoverNumberInput',
+  component: Component,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['!autodocs', 'ui'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {onChange: fn(), onSave: fn()},
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
+export const Default: Story = {
+  args: {
+    min: 1,
+    max: 100,
+    decimals: 0,
+  },
+};
